d3ed82fd4d2960f3fa5f3dfd48f6b57fe02f3f6b
Mike Flinn <mflinn92@gmail.com>
2019-10-10 14:27:20 -0400

    Defined schema for the product details collection

--- db/index.js
+++ db/index.js
@@ -1,5 +1,28 @@
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/BTetsy' ,{useNewUrlParser: true, useUnifiedTopology: true});
-const db = mongoose.connection;
-db.once('open', () => console.log('mongoose is connected'));
+
+const productDetailsSchema = new mongoose.Schema({
+  productId: {type: Number, index: {unique: true}},
+  sellerId: Number,
+  sellerName: String,
+  averageReviewScore: Number,
+  numberReviews: Number,
+  itemName: String,
+  badge: String,
+  sellerLocation: String,
+  itemPrice: Number,
+  freeShipping: Boolean,
+  productOptions: [
+    {
+      optionName: String,
+      choices: [
+        {choice: String, adjustedPrice: Number}
+      ]
+    }
+  ],
+  personalization: Boolean,
+  availableQuantity: Number,
+  onOrder: Number,
+});
+console.log(productDetailsSchema);
