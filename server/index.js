f08d22b367d0f201e732e34d17929885b8153093
Mike Flinn <mflinn92@gmail.com>
2019-10-11 15:05:09 -0400

    Set up basic route handler for a get request to retrieve product data. Currently prints request parameters only

--- server/index.js
+++ server/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const morgan = require('morgan');
 const PORT = 1234;
 
 const app = express();
 
+app.use(morgan());
 app.use(express.static('./public'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+app.get('/api/checkout/:productId/details', (req, res) => {
+  console.log(req.params);
+})
 
 app.listen(PORT, () => {
   console.log(`Listening at port ${PORT}`);
