89f3acf06f1931dcf3033a9caeb3dc9aab69d87a
Mike Flinn <mflinn92@gmail.com>
2019-10-14 12:45:07 -0400

    Added a test which confirms the server returns 404 not found for a get request with invalid product id

--- spec/server.test.js
+++ spec/server.test.js
@@ -1,5 +1,5 @@
 const productModel = require('../server/models.js');
-//const mongoose = require('mongoose');
+const axios = require('axios');
 
 describe('Product Model', () => {
 
@@ -12,3 +12,25 @@ describe('Product Model', () => {
   });
 
 });
+
+describe('API Routes', () => {
+  test('A get request to /api/checkout/:productId/details should return the requested product', async () => {
+    try {
+      var response = await axios.get('http://127.0.0.1:1234/api/checkout/3/details');
+    } catch (err){
+      console.error(err);
+    }
+    expect(response.data.productId).toBe(3);
+    expect(response.data.sellerName).toBe('Kai Dong');
+  });
+
+  test('A get request for an invalid product id should 404', async () => {
+    try {
+      var response = await axios.get('http://127.0.0.1:1234/api/checkout/2345/details');
+    } catch (err) {
+      var response = err;
+    }
+    expect(response.response.status).toBe(404);
+    expect(response.response.statusText).toBe('Not Found');
+  });
+})
