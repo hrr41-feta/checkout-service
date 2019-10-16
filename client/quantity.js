97e5686d5a78d10884f8a906df90af57c22617f2
Mike Flinn <mflinn92@gmail.com>
2019-10-16 11:55:19 -0400

    Formatting change for readability

--- client/quantity.js
+++ client/quantity.js
@@ -23,7 +23,11 @@ class Quantity extends React.Component {
         <label>Quantity</label><br />
         <select value={this.state.quantitySelection} onChange={this.handleDropDownSelection}>
           {quantities.map((quantity) => {
-            return <option value={quantity + 1} key={quantity} >{quantity + 1}</option>
+            return (<option
+            value={quantity + 1}
+            key={quantity} >
+              {quantity + 1}
+            </option>)
           })}
         </select>
       </div>
