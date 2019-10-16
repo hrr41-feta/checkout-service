64379f496b3d111d7b567b8e44e8130cf8e325e0
Mike Flinn <mflinn92@gmail.com>
2019-10-16 08:26:44 -0400

    Updated badge component to use inline conditionals. Reformatted html to be on multiple lines for readability.

--- client/onOrderAvailable.js
+++ client/onOrderAvailable.js
@@ -1,8 +1,16 @@
 import React from 'react';
 
 const OnOrderAvailable = ({availableQuantity, onOrder}) => {
-  let popularItem = <div className="popularItem"><em>Other people want this.</em> {onOrder} people have this in their carts right now.</div>
-  let limitedQuantity = <div className="limitedQuantity"><em>Almost Gone.</em> There's only {availableQuantity} left.</div>
+  let popularItem = (
+    <div className="popularItem">
+      <em>Other people want this.</em> {onOrder} people have this in their carts right now.
+    </div>
+  );
+  let limitedQuantity = (
+    <div className="limitedQuantity">
+      <em>Almost Gone.</em> There's only {availableQuantity} left.
+    </div>
+  );
   if (availableQuantity < 5) {
     return limitedQuantity;
   } if (onOrder >= 10) {
