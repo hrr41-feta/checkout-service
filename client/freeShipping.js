738fb61e03cfbcd6c93b07bfa23f6696b22bc6b0
Mike Flinn <mflinn92@gmail.com>
2019-10-16 08:19:43 -0400

    Updated free shipping component to use inline conditional rendering.

--- client/freeShipping.js
+++ client/freeShipping.js
@@ -1,14 +1,21 @@
 import React from 'react';
 
 const FreeShipping = ({freeShipping}) => {
-  let shippingOffered = <div className="freeShipping">Free shipping to <span>United States</span></div>
-  let shippingNotOffered = <div className="freeShipping"></div>
+  // let shippingOffered = <div className="freeShipping">Free shipping to <span>United States</span></div>
+  // let shippingNotOffered = <div className="freeShipping"></div>
 
-  if (freeShipping) {
-    return shippingOffered;
-  } else {
-    return shippingNotOffered;
-  }
+  // if (freeShipping) {
+  //   return shippingOffered;
+  // } else {
+  //   return shippingNotOffered;
+  // }
+  return (
+    <div className="freeShipping">
+      { freeShipping &&
+        <span>Free shipping to <span>United States</span></span>
+      }
+    </div>
+  );
 };
 
 export default FreeShipping;
