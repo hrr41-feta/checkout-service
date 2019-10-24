5c8595d552c24713ce959293f37b0bfc68aeba49
Mike Flinn <mflinn92@gmail.com>
2019-10-23 22:46:48 -0400

    Minor formatting adjusted to avoid missing space on page

--- client/components/freeShipping.js
+++ client/components/freeShipping.js
@@ -7,8 +7,7 @@ const FreeShipping = ({ freeShipping }) => {
       { freeShipping &&
         (
           <span>
-            Free shipping to
-            <span className={styles.freeShippingLocation}>United States</span>
+            Free shipping to <span className={styles.freeShippingLocation}>United States</span>
           </span>
         )}
     </div>
