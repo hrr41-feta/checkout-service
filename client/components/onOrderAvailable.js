794abfea549c6bba5e9818f36cb81092b591f112
Rob Nolan <rlnolan15@gmail.com>
2019-11-14 12:30:20 -0500

    take out link that I forgot ot comment outwq

--- client/components/onOrderAvailable.js
+++ client/components/onOrderAvailable.js
@@ -1,23 +1,34 @@
 /* eslint-disable react/jsx-filename-extension */
-import React from 'react';
-import styles from './styles.css';
+import React from "react";
+import styles from "./styles.css";
 
 const OnOrderAvailable = ({ availableQuantity, onOrder }) => {
   const popularItem = (
     <div className={styles.popularItem}>
-      <img src="https://moviepreview.s3.us-east-2.amazonaws.com/shoppingcard.png" className={styles.alertImage} />
-      <span><b>Other people want this.</b> {onOrder} people have this in their carts right now.</span>
+      <img
+        src="https://moviepreview.s3.us-east-2.amazonaws.com/shoppingcard.png"
+        className={styles.alertImage}
+      />
+      <span>
+        <b>Other people want this.</b> {onOrder} people have this in their carts
+        right now.
+      </span>
     </div>
   );
   const limitedQuantity = (
     <div className={styles.popularItem}>
-      <img src="https://moviepreview.s3.us-east-2.amazonaws.com/shoppingcard.png" className={styles.alertImage}></img>
-      <span><b>Almost Gone.</b> There's only {availableQuantity} left.</span>
+      <img
+        src="https://moviepreview.s3.us-east-2.amazonaws.com/shoppingcard.png"
+        className={styles.alertImage}
+      ></img>
+      <span>
+        <b>Almost Gone.</b> There's only {availableQuantity} left.
+      </span>
     </div>
   );
   if (availableQuantity < 5) {
     return limitedQuantity;
-  // eslint-disable-next-line no-else-return
+    // eslint-disable-next-line no-else-return
   } else if (onOrder >= 10) {
     return popularItem;
   } else {
@@ -26,5 +37,3 @@ const OnOrderAvailable = ({ availableQuantity, onOrder }) => {
 };
 
 export default OnOrderAvailable;
-
-https://moviepreview.s3.us-east-2.amazonaws.com/shoppingcard.png
