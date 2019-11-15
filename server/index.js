b3eb45b6b0c1eb404f58dee9895875889000b22d
Rob Nolan <rlnolan15@gmail.com>
2019-11-14 19:25:01 -0500

    add get req for loader.io

--- server/index.js
+++ server/index.js
@@ -37,6 +37,9 @@ app.get("/api/checkout/:id", postgreSQL.getProductById);
 app.post("/api/checkout/", postgreSQL.addProduct);
 app.put("/api/checkout/:id", postgreSQL.updateProductById);
 app.delete("/api/checkout/:id", postgreSQL.deleteProductById);
+app.get("/loaderio-c3fed30e7f6337ddb87a1485bd6623c9/", (req, res) => {
+  return "loaderio-c3fed30e7f6337ddb87a1485bd6623c9";
+});
 
 app.listen(PORT, () => {
   console.log(`Listening at port ${PORT}`);
